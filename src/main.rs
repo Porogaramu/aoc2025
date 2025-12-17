e2336f2522ccaad2648c59629d79ed87f9239ee2
Porogaramu <112273919+Porogaramu@users.noreply.github.com>
2025-12-17 18:34:38 -0500

    Added CLI  Support (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{env::args, path::PathBuf, process::exit};
+
+use aoc2025::run;
+
 fn main() {
-    println!("Hello, world!");
+    // Get Command Line Arguments
+    let mut args = args();
+    // Skip the executable file
+    args.next();
+    // Gets the day
+    let day = match args.next() {
+        // Parses the day number
+        Some(a) => match a.parse::<u32>() {
+            Ok(n) => n,
+            // If error, then print and exit
+            Err(e) => {
+                eprintln!("{e}");
+                exit(0);
+            }
+        },
+        None => {
+            eprintln!("No day inputted");
+            exit(0);
+        }
+    };
+    // Gets the file
+    let file = match args.next() {
+        Some(f) => PathBuf::from(f),
+        None => {
+            eprintln!("No file provided");
+            exit(0)
+        }
+    };
+    // Runs the day with the specified file
+    run(day, &file);
 }
